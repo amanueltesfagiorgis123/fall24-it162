1c30ae9b2f89ebc54d85049fada1b61ddeefd7cc
amanueltesfagiorgis123 <147437367+amanueltesfagiorgis123@users.noreply.github.com>
2024-12-11 16:24:39 -0800

    Add files via upload

---
+++ NNwebsite/footer.php
@@ -0,0 +1,19 @@
+<section id="footer-page" class="footer">
+    <div class="w-layout-blockcontainer container-3 w-container">
+      <div class="text-block-2">Copyright@ 2024 Nahome Negash . All rights reserved.<a id="html-checker" href="#" target="_blank">Check HTML</a> ~ 
+          <a id="css-checker" href="#" target="_blank">Check CSS</a></small> </div>
+    </div>
+  </section>
+  <script>//https://tinyurl.com/dynamic-html-css-checkers
+    document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+      
+    document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+     
+     //set current year in span with id of this-year
+     let d = new Date(); let thisYear = d.getFullYear();
+     document.getElementById("this-year").innerHTML = thisYear;</script>
+    
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=67328f25ac8a797b1bd8b5fa" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+</body>
+</html>

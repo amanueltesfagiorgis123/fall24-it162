924d4d2533be623cc7282aa96e40078b868e0ef4
amanueltesfagiorgis123 <147437367+amanueltesfagiorgis123@users.noreply.github.com>
2024-12-11 17:05:59 -0800

    Add files via upload

---
+++ it162/NNwebsite final/includes/portal-config.php
@@ -0,0 +1,41 @@
+<?php
+/*
+   portal-config.php
+
+
+   Used to store all PHP configuration information
+*/
+
+
+//prevents data from being sent early
+ob_start();
+//place site wide functions in a separate file
+include 'functions.php';
+
+//this helps us avoid PHP date errors:
+date_default_timezone_set('America/Los_Angeles');
+
+
+//creates a constant that stores the file name of the current page - cannot be changed
+define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
+
+
+//$title is a default variable we can change based on circumstances
+$title = THIS_PAGE;
+
+switch(THIS_PAGE){
+
+
+    case 'index.php':
+        $title = 'My Portal';
+        $logo = 'fa-home';
+        $PageID = 'Welcome';
+    break;
+
+    default:
+        $title = THIS_PAGE;
+        $logo = 'fa-home';
+        $PageID = 'No PageID created';
+   }
+   $nav1['index.php'] = "Home";
+  ?>
